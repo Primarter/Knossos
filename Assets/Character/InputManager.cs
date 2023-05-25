93edba229c2e7cf6a602f125bc2fc2d9c1d84725
Primarter <primarter@gmail.com>
2023-05-25 20:38:08 +0100

    Player Controller base

--- Assets/Character/InputManager.cs
+++ Assets/Character/InputManager.cs
@@ -8,13 +8,15 @@ public class InputManager : MonoBehaviour
     {
         public float horizontal;
         public float vertical;
+        public bool dash;
     }
 
     public static Inputs inputs;
 
     void Update()
     {
-        inputs.horizontal = Input.GetAxis("Horizontal");
-        inputs.vertical = Input.GetAxis("Vertical");
+        inputs.horizontal = Input.GetAxisRaw("Horizontal");
+        inputs.vertical = Input.GetAxisRaw("Vertical");
+        inputs.dash = Input.GetButtonDown("Dash");
     }
 }
