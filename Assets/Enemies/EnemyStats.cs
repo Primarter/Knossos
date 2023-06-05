d2dfcaea112191ef8d087b302d31b039ea6a0c38
Primarter <primarter@gmail.com>
2023-06-05 16:57:52 +0100

    Setup Enemy 2 for priority test in character controller

--- Assets/Enemies/EnemyStats.cs
+++ Assets/Enemies/EnemyStats.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class EnemyStats : ScriptableObject
 {
     [Range(0, 5)]
-    int detectionPriority = 0;
+    public int detectionPriority = 0;
+    public int life = 100;
+    public int damage = 3;
+    public int moveSpeed = 5;
 }
