6d831b58b31e7712a9f089b38979aa82cd9ca352
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2023-07-18 03:10:24 +0100

    Started wall grid visibility algorithm

--- Assets/Environment/Labyrinth/LabyrinthManager.cs
+++ Assets/Environment/Labyrinth/LabyrinthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Knossos.Map
 {
@@ -24,6 +25,8 @@ namespace Knossos.Map
         Grid grid;
         Transform playerTransform;
 
+        List<Cell> visibleCells = new();
+
         void Awake()
         {
             grid = GetComponent<Grid>();
@@ -33,11 +36,42 @@ namespace Knossos.Map
         void Start()
         {
             grid.cellSize = new Vector3(16f, 0f, 16f);
+
+            foreach (Cell cell in map)
+            {
+                // if (cell.type == 1)
+                cell.obj.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.ShadowsOnly; // ShadowCastingMode.On;
+            }
         }
 
         void Update()
         {
-            print( grid.WorldToCell(playerTransform.position) );
+            foreach (Cell cell in visibleCells)
+                cell.obj.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+            visibleCells.Clear();
+
+            Vector3Int gridPos3D = grid.WorldToCell(playerTransform.position);
+            Vector2Int gridPos = new Vector2Int(gridPos3D.x, gridPos3D.z);
+            int mapIndex = (gridPos.y * mapWidth) + gridPos.x;
+
+            if (gridPos.x < 0 || gridPos.x > mapWidth-1 || gridPos.y < 0 || gridPos.y > mapHeight-1) // if outside of map
+                return;
+            if (map[mapIndex].type == 1) // if inside a wall
+                return;
+
+
+            // TODO: go through each direction to until wall to find visible cells
+
+            // for (int i = 0 ; i < mapWidth ; ++i)
+            // {
+            //     int index = (gridPos.y * mapWidth) + (gridPos.x + i);
+            //     visibleCells.Add(map[index]);
+            //     if (map[index].type == 1)
+            //         break;
+            // }
+
+            // foreach (Cell cell in visibleCells)
+            //     cell.obj.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.On;
         }
     }
 
