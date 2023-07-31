38d5622d7d5346f38eda12291ebe3852f8307a88
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2023-07-31 17:40:06 +0100

    Fix firewall blocking navmesh when not activated

--- Assets/Environment/FireWall.cs
+++ Assets/Environment/FireWall.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.VFX;
 
 namespace Knossos.Environment
@@ -11,11 +12,14 @@ public class FireWall : MonoBehaviour
 {
     VisualEffect fire;
     BoxCollider boxCollider;
+    NavMeshObstacle navMeshObstacle;
 
     private void Awake()
     {
         fire = GetComponent<VisualEffect>();
         boxCollider = GetComponent<BoxCollider>();
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+
         if (transform.parent != null && transform.parent.tag == "Cluster")
         {
             var cluster = transform.parent.GetComponent<Enemies.Cluster>();
@@ -31,18 +35,21 @@ public class FireWall : MonoBehaviour
     {
         fire.Stop();
         boxCollider.enabled = false;
+        navMeshObstacle.enabled = false;
     }
 
     public void TurnOnWall()
     {
         fire.Play();
         boxCollider.enabled = true;
+        navMeshObstacle.enabled = true;
     }
 
     public void TurnOffWall()
     {
         fire.Stop();
         boxCollider.enabled = false;
+        navMeshObstacle.enabled = false;
     }
 }
 
