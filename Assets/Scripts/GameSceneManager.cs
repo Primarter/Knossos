d7183d03d24d8dde436e2798608cd46b52429a17
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2023-08-12 15:49:02 +0100

    added pause menu

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Build Menu");
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Build Scene");
